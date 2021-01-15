4a2f239d1f14d685352d32c784463d2999c4cf81
lssgt <lssgt@DESKTOP-C4PDKK0>
2021-01-15 18:23:32 +0900

    0115카랜다과제시작

---
+++ HomWork_2/src/Calenader_Swing.java
@@ -0,0 +1,17 @@
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class Calenader_Swing extends JFrame implements ActionListener {
+	
+	public Calenader_Swing() {
+		
+	}
+
+	public static void main(String[] args) {
+		new Calenader_Swing();
+		
+
+	}
+
+}

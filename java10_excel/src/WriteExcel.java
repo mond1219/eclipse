d500c9354a3ee66057818d47624058252bae5c65
lssgt <lssgt@DESKTOP-C4PDKK0>
2021-01-21 10:10:11 +0900

    0121

---
+++ java10_excel/src/WriteExcel.java
@@ -0,0 +1,20 @@
+//자바에서 excel로 쓰기 읽기 
+//jakerta.apache.org에서 POI를 다운로드 받는다.abstract
+//poi-bin-5.0.0.jar,
+//buildPath 설정 ->상단 project에서 마우스 오른쪽 -> buildPath선택-> library 외부 add
+
+
+public class WriteExcel {
+
+	public WriteExcel() {
+		//엑셀로 쓰기
+		
+		//1.workbook객체 생성
+	}
+
+	public static void main(String[] args) {
+		new WriteExcel();
+
+	}
+
+}

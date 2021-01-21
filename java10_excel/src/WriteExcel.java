60653edb5b17b7441e4ce8c3aa40bf640a53247a
lssgt <lssgt@DESKTOP-C4PDKK0>
2021-01-21 10:14:58 +0900

    0121

--- java10_excel/src/WriteExcel.java
+++ java10_excel/src/WriteExcel.java
@@ -1,3 +1,7 @@
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
 //자바에서 excel로 쓰기 읽기 
 //jakerta.apache.org에서 POI를 다운로드 받는다.abstract
 //poi-bin-5.0.0.jar,
@@ -10,6 +14,13 @@ public class WriteExcel {
 		//엑셀로 쓰기
 		
 		//1.workbook객체 생성
+		 HSSFWorkbook workbook = new HSSFWorkbook();
+		 //2.sheet 객체 생성
+		 HSSFSheet sheet1 = workbook.createSheet("회원목록");
+		 HSSFSheet sheet2 =workbook.createSheet();
+		 //3.row객체 생성, 행구하기
+		 HSSFRow row1 = sheet1.createRow(0);//(0)행번호
+		 
 	}
 
 	public static void main(String[] args) {

c91a9c2018410df8cf0372c8a42114a8ed152853
lssgt <lssgt@DESKTOP-C4PDKK0>
2021-01-27 18:29:13 +0900

    0127

---
+++ HomeWork02/src/member/MemberDAO.java
@@ -0,0 +1,17 @@
+package member;
+
+public class MemberDAO extends DBConn {
+
+	public MemberDAO() {
+		//레코드 추가
+		//레코드 수정
+		//레코드 삭제
+		//레코드 검색
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}

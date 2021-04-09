b64336fe65871811978a6cf0decd01671efde884
mond1219 <76996008+mond1219@users.noreply.github.com>
2021-04-09 16:57:29 +0900

    Delete bitcamp02 directory

--- bitcamp02/src/main/java/com/bitcamp/jdbc/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.bitcamp.jdbc;
-
-import org.springframework.jdbc.core.JdbcTemplate;
-
-public class Constants {
-	//servlet-context.xml에 있는 객체를 자바로 가져오는 작업
-	public static JdbcTemplate jdbcTemplate =null;
-}
